1913edf1027486cf39956edbf61ede8834321c9b
Anton A Shanaurin <grspectre@gmail.com>
2023-09-21 20:34:56 +0300

    merge_with_tag.py

---
+++ merge_with_tag.py
@@ -0,0 +1,28 @@
+import subprocess
+
+
+def execute(command: str) -> str:
+    output = subprocess.check_output(command)
+    output = output.decode('utf8')
+    return output.strip()
+
+
+def get_head_commit() -> str:
+    return execute('git rev-parse --short HEAD')
+
+
+def main() ->  None:
+    branch = execute('git branch --show-current')
+    if branch != 'prd':
+        print("Branch {}. Must be prd".format(branch))
+        return
+    prd_head_commit = get_head_commit()
+    execute('git merge dev')
+    cur_head_commit = get_head_commit()
+    if cur_head_commit == prd_head_commit:
+        print('prd last commit and dev last dev commit are identical. exit')
+        return
+    execute('git tag {}'.format(cur_head_commit))
+
+if __name__ == '__main__':
+    main()
